8a1bb3575d1d8d85c0f26f3a0c6ff6b6a9424ef5
Tino Keinänen <98185741+Ke1in@users.noreply.github.com>
2026-09-12 05:35:00 +0300

    Tehtävä 10 valmis

---
+++ src/auto-api/api.php
@@ -0,0 +1,17 @@
+<?php
+
+$pdo = new PDO(
+    "mysql:host=db;dbname=autokanta;charset=utf8mb4",
+    "root",
+    "root"
+);
+
+$sql = "SELECT * FROM autot";
+
+$stmt = $pdo->query($sql);
+
+$autot = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+header("Content-Type: application/json");
+
+echo json_encode($autot);
